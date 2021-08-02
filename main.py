e50d58f074c9eb4d0e701da55f46c7b10fd03b2d
Dan <newwby@gmail.com>
2021-08-02 10:11:31 +0100

    commenting

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
-# from os import listdir
-# from os.path import isfile, join
-#
-# my_path = input()
-#
-# #debug_path = "C:\Users\newwb\Documents\Godot\project folders\1gamJam_june21_projectptp"
-#
-# only_files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
-#
-# print(only_files)
 
 from os import walk
 
-my_path = r"C:\Users\newwb\Documents\Godot\project folders\1gamJam_june21_projectptp"
+# given_path = input()
+given_path = r"C:\Users\newwb\Documents\Godot\project folders\1gamJam_june21_projectptp"
 
+# find everything in the given path
 gd_file_list = {}
-for (dirpath, dirnames, filenames) in walk(my_path):
+for (directory_path, directory_names, file_names) in walk(given_path):
 
-    for file in filenames:
+    for file in file_names:
+        # we're only interested in files that follow this format
         file_name = file[:-3]
         file_extension = file[-3:]
+        # if the extension (ending characters) isn't a three character sequence of '.gd', ignore it
         if file_extension == ".gd":
             # escape the backslash or it is treated as an escape sequence (causing EOF error)
-            file_path = dirpath + "\\" + file
+            file_path = directory_path + "\\" + file
             gd_file_list[file_path] = file_name
             # gd_file_list.append(file_path)
 
+# now we've separated the .gd files, open the relevant files to get contents
 for gdscript_path in gd_file_list:
     print("file name: ", gd_file_list[gdscript_path])
     print("absolute path: ", gdscript_path)
     # print(item)
     with open(gdscript_path) as gdscript:
         gdscript_contents = gdscript.readlines()
+        # entire contents of .gd file, split into a list by line
         print(gdscript_contents)
+    # linebreak
     print()
