fccb308ccc99ae4d346a56759b14f3d161477ba6
Dan <newwby@gmail.com>
2021-08-02 11:30:59 +0100

    user input validation, commenting

--- constant.py
+++ constant.py
@@ -1,2 +1,6 @@
 
+# disable this on live release
+DEBUG_FILE_PATH = True
+
+# disable this on live release, add as optional parameter for user
 COUNT_CODE_LINES = True
--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 
+# imported files and packages
 import constant
-from os import walk
-
-# TODO >> in live version should be a given input
-# should also handle incorrect input & loop until valid (with escape command)
-# add basic gui?
-
-# given_path = input()
-
-# raw string to prevent escape sequence errors
-given_path = r"C:\Users\newwb\Documents\Godot\project folders\1gamJam_june21_projectptp"
+from os import path, walk
 
 
-# this function scans a folder and all sub-folders for every file,
+# functions first, main body after
+#####
+# this function scans a passed folder and all sub-folders for every file,
 # before identifying whether a file is valid or invalid for inclusion in the returned dictionary
 # returned dictionary includes the file path (as a string) and name of the file without its file extension
 # this function is intended for use with the 'get_file_contents' function below but is functionally standalone
-def get_valid_files(valid_extension=".gd"):
+def get_valid_files(given_path, valid_extension=".gd"):
     # find everything in the given path
     full_file_list = {}
     for (directory_path, directory_names, file_names) in walk(given_path):
@@ -38,6 +32,7 @@ def get_valid_files(valid_extension=".gd"):
     return full_file_list
 
 
+#####
 # this function reads multiple files and yields their contents alongside an identifier for the file
 # the given identifier
 # this function can read a list of file paths, or a dict whose keys are file paths
@@ -64,6 +59,7 @@ def get_file_contents(gd_file_list, print_instruction=False):
                 print(gdscript_contents, "\n")
 
 
+#####
 # pass this function a body of gdscript formatted code (as a list)
 # it is intended for use alongside the above 'get_file_contents' function
 # without additional parameters this function will simply return the length of a list
@@ -84,23 +80,47 @@ def get_code_length(code_to_parse, remove_null_lines=False):
 #######################################################################################################################
 
 # main body
+# variables for code line counting
 total_lines_of_code_with_empty_lines = 0
 total_lines_of_code_without_null_lines = 0
+# variables for godot repository folder paths
+# debug value so don't have to keep typing in whilst testing
+# raw string to prevent escape sequence errors
+debug_repo_path = r"C:\Users\newwb\Documents\Godot\project folders\testproj_dodgecreeps"
+# else use default blank value and input
+repo_path = ""
 
-for code_body in get_file_contents(get_valid_files(), False):
-    # if count total lines of code is enabled, must collect value of total lines
+# validation for user
+# make sure they can exit the program if they wish
+# validate a passed folder path, make sure it is valid before continuing
+# make this its own function to clean up main?
+while not path.exists(repo_path):
+    print("To get a code line count, enter your godot folder path.")
+    print("Alternatively, type 'q', 'quit', or 'exit', to stop the program.")
+    repo_path = input("Please enter an input now: ")
+    if repo_path.lower() in ["q", "quit", "exit"]:
+        print("Exiting!")
+        break
+    print("Given file path is not valid!\n")
+
+# if repository path exists can begin running program functions on it
+if path.exists(repo_path):
+    print("\nFile Path:", repo_path)
+    # yield/generator of the contents of every .gd file inside the folder and its sub-folders
+    for code_body in get_file_contents(get_valid_files(repo_path), False):
+        # if count total lines of code is enabled, must collect value of total lines
+        if constant.COUNT_CODE_LINES:
+            total_lines_of_code_with_empty_lines += get_code_length(code_body, False)
+            total_lines_of_code_without_null_lines += get_code_length(code_body, True)
+
+        # TODO >> (deco?) extension for get_valid_files to collect all file names
+        # TODO >> introduce function for identifying 'func' lines inside gdscripts
+        # TODO >> identify all function calls within every file
+        # TODO >> list all function calls inside a file as a separate list
+
+    # if count total lines of code is enabled, must print an output of the count
     if constant.COUNT_CODE_LINES:
-        total_lines_of_code_with_empty_lines += get_code_length(code_body, False)
-        total_lines_of_code_without_null_lines += get_code_length(code_body, True)
-
-    # TODO >> (deco?) extension for get_valid_files to collect all file names
-    # TODO >> introduce function for identifying 'func' lines inside gdscripts
-    # TODO >> identify all function calls within every file
-    # TODO >> list all function calls inside a file as a separate list
-
-# if count total lines of code is enabled, must print an output of the count
-if constant.COUNT_CODE_LINES:
-    print("Total Lines of Code in Godot Repo (inc. all lines): "
-          + str(total_lines_of_code_with_empty_lines))
-    print("Total Lines of Code in Godot Repo (exc. empty or commented out lines): "
-          + str(total_lines_of_code_without_null_lines))
+        print("Total Lines of Code in Godot Repo (inc. all lines): "
+              + str(total_lines_of_code_with_empty_lines))
+        print("Total Lines of Code in Godot Repo (exc. empty or commented out lines): "
+              + str(total_lines_of_code_without_null_lines))
